6ed2ea37ed3cf80981b8a0404ff8912f26cb5c19
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-08-11 12:14:43 +0300

    HW10
    HW10 commit1

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,17 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("properties")
+public class SystemPropertiesTests {
+
+    @Test
+    void test1() {
+
+        System.out.println("Test1 browser   " + System.getProperty("browser"));
+
+
+    }
+
+}
