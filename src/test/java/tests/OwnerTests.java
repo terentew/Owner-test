07a657a55d1dc09ccb60c15e7f0fce0546f84ec7
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-08-12 13:23:55 +0300

    Commit 6 HW10

--- src/test/java/tests/OwnerTests.java
+++ src/test/java/tests/OwnerTests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static config.Credentials.credentials;
 
+@Tag("properties")
 public class OwnerTests {
 
     @Test
+
     void readFromPropertiesTest() {
         String login = credentials.login();
         String password = credentials.password();
